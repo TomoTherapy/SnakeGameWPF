4620161dd4c94bce3ea547af20558f1af0f4f876
Joongi Hong <tomotherapy@outlook.com>
2024-04-16 23:48:00 +0900

    b

--- SnakeGameWPF/SnakeGameWPF/MainWindow.xaml.cs
+++ SnakeGameWPF/SnakeGameWPF/MainWindow.xaml.cs
@@ -1,25 +1,12 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Xml.Linq;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace SnakeGameWPF
 {
